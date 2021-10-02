5fd6029320f1747ca66bec1443b68bed40d91e1d
Mert Koprucu <mert.koprucu@siemens.com>
2021-10-02 23:29:37 +0300

    Introduce ApiKeyAuthAttribute.

---
+++ API/Attributes/ApiKeyAuthAttribute.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Attributes
+{
+    [AttributeUsage(validOn: AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKeyAuthAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string API_KEY_NAME = "ApiKey";
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            StringValues extractedApiKeyValue;
+            var result = context.HttpContext.Request.Headers.TryGetValue(API_KEY_NAME, out extractedApiKeyValue);
+
+            if(!result)
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Api Key was not provided"
+                };
+                return;
+            }
+
+            var appSettings = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+
+            var apiKey = appSettings.GetValue<string>(API_KEY_NAME);
+
+            if(!apiKey.Equals(extractedApiKeyValue))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Api Key is not valid"
+                };
+                return;
+            }
+
+            await next();
+
+        }
+    }
+}
