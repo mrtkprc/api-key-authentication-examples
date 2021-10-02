c116cee328a8454eb3b28a20855d40d4823b285b
Mert Koprucu <mert.koprucu@siemens.com>
2021-10-02 23:29:59 +0300

    Secure endpoints with ApiKeyAuth Attribute.

---
+++ API/Controllers/SecurePointController.cs
@@ -0,0 +1,26 @@
+﻿using API.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiKeyAuth]
+    [ApiController]
+    [Route("[controller]")]
+    public class SecurePointController : ControllerBase
+    {
+        public SecurePointController()
+        {
+
+        }
+
+        [HttpGet("confidential")]
+        public IActionResult GetConfidentialData()
+        {
+            return Ok("It is Ok.");
+        }
+    }
+}
